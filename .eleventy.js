e42b21d901317e12eeb62e5e3c07a288837559a4
Ishaat Chowdhury <ishaat@ualberta.ca>
2020-05-23 17:08:34 -0600

    Inline css

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,6 @@ const pluginPWA = require("eleventy-plugin-pwa");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/img");
-    eleventyConfig.addPassthroughCopy("src/assets/css/prism-tomorrow-night.css")
     eleventyConfig.addPassthroughCopy("src/manifest.json");
     eleventyConfig.addPassthroughCopy("src/robots.txt");
     eleventyConfig.addPassthroughCopy("src/admin/config.yml");
@@ -18,10 +17,6 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addCollection("posts", function(collection) {
         return collection.getFilteredByGlob("src/posts/*.md");
-    });
-
-    eleventyConfig.setBrowserSyncConfig({
-        files: ["dist/assets/css"],
     });
 
     eleventyConfig.addPlugin(syntaxHighlight);
